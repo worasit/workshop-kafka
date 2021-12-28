be4dbbf855afe49158155b9f493f2fb630910a3d
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-28 13:59:09 +0700

    Added kafka topic's commands

---
+++ scripts/001-topic.sh
@@ -0,0 +1,16 @@
+# Create a topic
+kafka-topics --bootstrap-server localhost:9092 --create \
+             --topic learning.helloworld \
+             --partitions 3 \
+             --replication-factor 1
+
+# List topics
+kafka-topics --bootstrap-server localhost:9092 --list
+
+# Describe a topic
+kafka-topics --bootstrap-server localhost:9092 --describe \
+             --topic learning.helloworld
+
+# Delete a topic
+kafka-topics --bootstrap-server localhost:9092 --delete \
+             --topic learning.helloworld
