cc28027457a14fb773207916638b255f1013d58b
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-28 22:23:43 +0700

    Added Kafka's java example project.

---
+++ kafka-java/src/main/java/org/daimongkol/kafka/ProducerDemo.java
@@ -0,0 +1,10 @@
+package org.daimongkol.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ProducerDemo {
+    public static void main(String[] args) {
+        log.info("Hello world");
+    }
+}
