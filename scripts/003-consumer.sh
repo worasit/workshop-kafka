9837d09ccf5cd69e39ca34ab80840a8edba0108d
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-28 22:22:59 +0700

    Added kafka'security configuration.

--- scripts/003-consumer.sh
+++ scripts/003-consumer.sh
@@ -1,8 +1,15 @@
 https://kafka.apache.org/documentation/#consumerconfigs
 
-# Messages would not show in order, since we have 3 partitions
+# Messages would not show in order, since we have 3 partitions (Random consumer-id)
 kafka-console-consumer --bootstrap-server localhost:9092 \
                        --topic learning.helloworld \
                        --from-beginning # Optional
 
 # NOTE: You 1 partition to guarantee this
+
+
+kafka-console-consumer --bootstrap-server localhost:9092 \
+                       --topic learning.helloworld \
+                       --group "consumer.group.1"
+                       --from-beginning # Optional
+
