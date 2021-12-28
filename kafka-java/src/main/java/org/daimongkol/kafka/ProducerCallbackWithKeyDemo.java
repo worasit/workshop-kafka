1e86580e426d997e170f2f2d099fc5103c897bbf
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-28 22:53:35 +0700

    Added producer source code.

---
+++ kafka-java/src/main/java/org/daimongkol/kafka/ProducerCallbackWithKeyDemo.java
@@ -0,0 +1,52 @@
+package org.daimongkol.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Date;
+import java.util.Properties;
+
+@Slf4j
+public class ProducerCallbackWithKeyDemo {
+
+    public static final String TOPIC = "learning.helloworld";
+
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create producer producerProps
+        // https://kafka.apache.org/documentation/#producerconfigs
+        Properties producerProps = new Properties();
+        producerProps.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        producerProps.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(producerProps);
+
+        // Send data
+        for (int i = 0; i < 100; i++) {
+            // Build a record
+            final String MESSAGE = "this is from java producer " + new Date();
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, MESSAGE);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        log.info("topic: " + recordMetadata.topic());
+                        log.info("partition: " + recordMetadata.partition());
+                        log.info("offset: " + recordMetadata.offset());
+                        log.info("timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        log.error("Error while producing", e);
+                    }
+                }
+            });
+
+            Thread.sleep(1000);
+        }
+
+        producer.flush();
+        producer.close();
+
+    }
+}
--- kafka-java/src/main/java/org/daimongkol/kafka/ProducerDemo.java
+++ kafka-java/src/main/java/org/daimongkol/kafka/ProducerDemo.java
@@ -1,10 +1,38 @@
 package org.daimongkol.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Date;
+import java.util.Properties;
 
 @Slf4j
 public class ProducerDemo {
+
+    public static final String TOPIC = "learning.helloworld";
+    public static final String MESSAGE = "this is from java producer " + new Date();
+
     public static void main(String[] args) {
-        log.info("Hello world");
+        // Create producer producerProps
+        // https://kafka.apache.org/documentation/#producerconfigs
+        Properties producerProps = new Properties();
+        producerProps.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        producerProps.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProps.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(producerProps);
+
+        // Build a record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, MESSAGE);
+
+        // Send data
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
     }
 }
