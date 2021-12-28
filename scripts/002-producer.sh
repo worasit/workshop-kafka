6a31db2752a8246aa8e4d869910f5a0f5422c846
worasit <worasit.daimongkol@thoughtworks.com>
2021-12-28 14:06:21 +0700

    Added kafka producer's commands

---
+++ scripts/002-producer.sh
@@ -0,0 +1,4 @@
+# Start producing messages to Kafka cluster
+kafka-console-producer --bootstrap-server localhost:9092 \
+                       --topic learning.helloworld \
+                       --producer-property "acks=all"
